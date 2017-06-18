470e1ac041f2c7e4171a2e3f482d85d128ae5e3b
Vishal Khatal <vishal.khatal2206@gmail.com>
2017-06-18 15:54:17 +0530

    Added packges

--- RealEstate.API/RealEstate.API/DataAccesss/RealEstateContext.cs
+++ RealEstate.API/RealEstate.API/DataAccesss/RealEstateContext.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
-
+using RealEstate.API.Models;
 namespace RealEstate.API.DataAccesss
 {
-    public class RealEstateContext
+    public class MyDbContextInitializer : DropCreateDatabaseIfModelChanges<RealEstateContext>
     {
+        protected override void Seed(RealEstateContext dbContext)
+        {
+            // seed data
+            User user = new User();
+            user.FirstName = "Vishal";
+            user.LastName = "Khatal";
+
+            dbContext.User.Add(user);
+            dbContext.SaveChanges();
+            base.Seed(dbContext);
+        }
+    }
+
+    public class RealEstateContext : Microsoft.AspNet.Identity.EntityFramework.IdentityDbContext<ApplicationUser>
+    {
+        public RealEstateContext()
+            : base("RealEstateContext", throwIfV1Schema: false)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        #region Application Table
+        public DbSet<User> User { get; set; }
+        public DbSet<Property> Property { get; set; }
+        public DbSet<PropertyPhoto> PropertyPhotos { get; set; }
+        public DbSet<PropertyFeedBack> PropertyFeedBack { get; set; }
+        public DbSet<Message> Message { get; set; }
+        public DbSet<NewsLetter> NewsLetter { get; set; }
+        public DbSet<NewsLetterSubscriber> NewsLetterSubscriber { get; set; }
+             #endregion
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           // Database.SetInitializer(new MigrateDatabaseToLatestVersion<RealEstateContext, RealEstate.API.Migrations.Configuration>());
+            modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
+
+            //Database.SetInitializer(new MyDbContextInitializer());
+            base.OnModelCreating(modelBuilder);
+        }
+        public static RealEstateContext Create()
+        {
+            return new RealEstateContext();
+        }
     }
 }
